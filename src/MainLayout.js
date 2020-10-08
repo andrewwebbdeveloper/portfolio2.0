666dd6041d92c6912b6669781e88fb51dd5f6ac7
andrewwebbdeveloper <drewbird87@gmail.com>
2020-10-07 21:49:35 -0400

    Update MainLayout.js
    Switch MainView display to block on small screens so sections remain centered.

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
+import device from './utilities/media';
 
 const MainLayout = styled.main`
   display: grid;
   grid-auto-flow: row;
   max-width: 100%;
   overflow: hidden;
+
+  @media ${device.tablet} {
+    display: block;
+  }
+  @media ${device.desktop} {
+  }
+  @media ${device.desktopL} {
+  }
 `;
 
 export default MainLayout;
